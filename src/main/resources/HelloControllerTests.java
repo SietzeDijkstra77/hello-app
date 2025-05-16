c4d1c4d61c614c70df545b2ee84f33838437ee04
Sietze Dijkstra <sietze.dijkstra@the-experts.nl>
2025-05-16 13:06:55 +0200

    pass failing test

--- src/main/resources/HelloControllerTests.java
+++ src/main/resources/HelloControllerTests.java
@@ -1,5 +1,3 @@
-package com.mkyong;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
